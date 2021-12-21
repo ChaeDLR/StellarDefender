23c97ef88392df3cabf4aa7affbb8d64c5a0cf63
ChaeDLR <delarosa.chae@gmail.com>
2021-12-21 11:12:13 -0500

    still working

--- game/asset_manager.py
+++ game/asset_manager.py
@@ -9,30 +9,34 @@ class Assets:
     __sprite_images: dict = {}
 
     def __init__(self):
-        file_names: list = os.listdir(self.__assets_directory)
-        colorkey: bool = True
+        self.__load(self.__assets_directory)
+
+    def __load(self, path: str, imgs_dict: dict) -> None:
+        """Load __sprite_images: dict with all the images in the path arg directory"""
         img_size: tuple = (64, 64)
-
-        for file_str in file_names:
-            if file_str[len(file_str) - 4 :] == ".png":
-                continue
+        for file in os.listdir(path):
+            if file[len(file) - 4 :] == ".png":
                 # animation types
-                file_name_altered: str = file_str[:-4]
+                str_parts = img[:-4].split("_")
 
-                img: Surface = self.__get_image(
-                    file_name=file_str, resize=img_size, colorkey=colorkey
-                )
+                img: Surface = self.__load_image(file_name=file, resize=img_size)
                 img.__setattr__("colors", self.__get_sprite_colors(img))
 
-                self.__sprite_images[file_name_altered] = img
-
+                if imgs_dict:
+                    imgs_dict[str_parts[0]] = img
+                else:
+                    self.__sprite_images[str_parts[0]] = img
             else:
-                str_parts = img[:-4].split("_")
-                ship_type: dict = {}
-                for img in os.listdir(os.path.join(self.__assets_directory, file_str)):
-                    print(f"Parts = {str_parts}")
 
-    def __get_image(self, file_name: str, resize: tuple = None, colorkey: bool = True):
+                # for img in os.listdir(os.path.join(self.__assets_directory, file)):
+                #     print(f"Parts = {str_parts}")
+                # Grab key
+                key: str = file
+                # recall
+                print(os.path.abspath(os.path.join(path, file)))
+                # self.__load(os.path.abspath(os.path.join(path, file)))
+
+    def __load_image(self, file_name: str, resize: tuple[int, int] = None):
         """
         get an image from assests
         file_name: string -> file location
@@ -40,11 +44,10 @@ class Assets:
         """
         img = image.load(os.path.join(self.__assets_directory, file_name)).convert()
 
-        if colorkey:
-            # get color of top left pixel
-            colorkey = img.get_at((0, 0))
-            # assign the file image
-            img.set_colorkey(colorkey)
+        # get color of top left pixel
+        colorkey = img.get_at((0, 0))
+        # assign the file image
+        img.set_colorkey(colorkey)
 
         if resize:
             img = transform.scale(img, resize)
@@ -64,6 +67,12 @@ class Assets:
                     colors.append(rgb)
         colors.sort(key=sum)
         return colors
+
+    def get_image(cls, key: str) -> Surface or dict:
+        try:
+            return cls.__sprite_images[key]
+        except KeyError as ex:
+            raise ex.with_traceback()
 
 
 if __name__ == "__main__":
